8f6cd73cb25e4867b9eabd0aefd7003fd5f3c31c
VAMSHIDHAR DAWOOR <vamshidhar.20bce7285@vitap.ac.in>
2023-02-17 21:10:17 +0530

    jwt created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,9 @@
 const User = require('../models/userModels')
+const jwt = require('jsonwebtoken')
 
-
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
 
 // login user
 const loginUser = async (req, res) => {
@@ -15,11 +18,14 @@ const signupUser = async (req, res) => {
     try{
         const user = await User.signup(email, password) 
 
-        res.status(200).json({email, user})
+        // create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
     }catch(error){
         res.status(400).json({error: error.message})
     }
 }
 
  
-module.exports = { signupUser, loginUser }
+module.exports = { signupUser, loginUser,  }
